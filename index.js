1c08e60a9ee1a6c22210508d8b65bb9e40317f4b
Olabisi Ajayi <ajayiolabisi.234@gmail.com>
2024-02-12 15:00:35 -0800

    New changes made to media querry

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("parent: ", document.body.parentNode);
+
+
+// // On focus of input, alert "You're editing the name field!"
+document.querySelectorAll('input').forEach((input) => {
+    input.addEventListener('click', (e) => {
+        alert(`You are editing the ${e.target.name} field!`);
+    });
+})
+
+// On send message, alert "Your message has been sent!"
+document.querySelector('#contactform').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert("Your message has been sent!");
+})
